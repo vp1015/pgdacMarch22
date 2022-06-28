676df01a8d4fffa52d34270c3620655c2b9aceb2
vp96 <vinayakpatil209@gmail.com>
2022-06-28 18:11:40 +0530

    file 1 add

---
+++ B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class B
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("SIZE OF ARRAY");
+n =sc.nextInt();
+int a[]=new int[n];
+System.out.println("ENTER ELEMENTS OF ARRAY");
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+a
